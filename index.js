ecda01b5406ac412cdc6415fbd67a7510e1023b9
Ripassorker <ripassorker02@glail.com>
2023-05-11 01:20:32 +0600

    added project post route

--- index.js
+++ index.js
@@ -11,40 +11,53 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvjclco.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
-  try {
-    const projectsCollection = client.db("portfoliyo").collection("projects");
+   try {
+      const projectsCollection = client.db("portfoliyo").collection("projects");
 
-    app.get("/projects", async (req, res) => {
-      const result = await projectsCollection.find({}).toArray();
-      res.send(result);
-    });
+      app.get("/projects", async (req, res) => {
+         const result = await projectsCollection
+            .find({})
+            .sort({ $natural: -1 })
+            .toArray();
+         res.send(result);
+      });
+      app.get("/project/:type", async (req, res) => {
+         const filter = { projectType: req.params.type };
+         const result = await projectsCollection
+            .find(filter)
+            .sort({ $natural: -1 })
+            .toArray();
+         res.send(result);
+      });
 
-    app.get("/projects/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: ObjectId(id) };
-      const result = await projectsCollection.find(filter).toArray();
-      res.send(result);
-    });
+      app.get("/projects/:id", async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: ObjectId(id) };
+         const result = await projectsCollection.find(filter).toArray();
+         res.send(result);
+      });
 
-    console.log("db connented");
-  } finally {
-  }
+      app.post("/project", async (req, res) => {
+         const result = await projectsCollection.insertOne(req.body);
+         res.send(result);
+      });
+   } finally {
+   }
 }
 
 run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Server is running...");
+   res.send("Server is running...");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running...on ${port}`);
+   console.log(`Server is running...on ${port}`);
 });
